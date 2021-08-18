251eee834eff685a905b154a3cd05c54eb9bbaea
avocadojesus <avocadojesus>
2021-08-18 11:02:33 -0700

    ensure fields are extracted from new payload shape

--- lib/omniauth/strategies/withings2.rb
+++ lib/omniauth/strategies/withings2.rb
@@ -9,14 +9,17 @@ module OmniAuth
         :site          => 'https://account.withings.com',
         :authorize_url => 'https://account.withings.com/oauth2_user/authorize2',
         :token_url     => 'https://wbsapi.withings.net/v2/oauth2',
+        extract_access_token: proc { |client, hash|
+          ::OAuth2::Client::DEFAULT_EXTRACT_ACCESS_TOKEN.call(client, hash['body'])
+        }
       }
 
       option :response_type, 'code'
       option :authorize_options, %i(scope response_type redirect_uri)
-      option :access_token_params, { action: 'requesttoken' }
 
       def build_access_token
         options.token_params.merge!(
+          action: 'requesttoken',
           headers: { 'Authorization' => basic_auth_header }
         )
         super
@@ -26,11 +29,11 @@ module OmniAuth
         "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
       end
 
-      # def query_string
-      #   # Using state and code params in the callback_url causes a mismatch with
-      #   # the value set in the withings2 application configuration, so we're skipping them
-      #   ''
-      # end
+      def query_string
+        # Using state and code params in the callback_url causes a mismatch with
+        # the value set in the withings2 application configuration, so we're skipping them
+        ''
+      end
 
       uid do
         access_token.params['userid']
